a4a0e6f570e9dc74e686918f5e47548ef31b3e74
GreaterWMS <38828089+Singosgu@users.noreply.github.com>
2025-09-19 10:18:41 +0800

    v4

--- launcher.py
+++ launcher.py
@@ -5,6 +5,7 @@ import uvicorn
 import time
 import socket
 import webbrowser
+import threading
 from bomiot_token import encrypt_info
 from bomiot.cmd.killport import kill_process_on_port
 from os.path import join, exists 
@@ -137,23 +138,36 @@ if __name__ == "__main__":
 
     # 启动 Django 开发服务器
     os.environ.setdefault("IS_LAN", "true")
-    uvicorn.run(
-        "bomiot_asgi:application",
-        host='0.0.0.0',
-        port=8008,
-        workers=1,
-        log_level="info",
-        uds=None,
-        ssl_keyfile=None,
-        ssl_certfile=None,
-        proxy_headers="store_true",
-        http="httptools",
-        server_header=False,
-        limit_concurrency=1000,
-        backlog=128,
-        timeout_keep_alive=5,
-        timeout_graceful_shutdown=30,
-        loop="auto",
-    )
+    def run_server():
+        uvicorn.run(
+            "bomiot_asgi:application",
+            host='0.0.0.0',
+            port=8008,
+            workers=1,
+            log_level="info",
+            uds=None,
+            ssl_keyfile=None,
+            ssl_certfile=None,
+            proxy_headers="store_true",
+            http="httptools",
+            server_header=False,
+            limit_concurrency=1000,
+            backlog=128,
+            timeout_keep_alive=5,
+            timeout_graceful_shutdown=30,
+            loop="auto",
+        )
+    run_server_thread = threading.Thread(target=run_server, daemon=True)
+    run_server_thread.start()
+    print('系统启动成功')
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    ip = s.getsockname()[0]
+    print('本机IP地址为:', ip)
+    s.close()
+    baseurl = "http://" + ip + ":8008"
+    print('浏览器正在打开:', baseurl)
+    webbrowser.open(baseurl)
+    
     
     
